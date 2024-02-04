5394a9d18ebf297e5e610ccf1f2418874bfe4d0b
bkmashiro <dylan_233@foxmail.com>
2024-02-04 11:43:43 +0800

    add user input

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public void Look(InputAction.CallbackContext value)
+    {
+        Vector2 input = value.ReadValue<Vector2>();
+        transform.eulerAngles += new Vector3(-input.y, input.x, 0);
+    }
+}
